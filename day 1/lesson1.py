4f4e00fb2413390b9a8e54986a2d4f329e7888e7
tazogogishvili <tazogogishvili25@gmail.com>
2024-01-05 00:04:10 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,37 @@
+
+#name = "tazo"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "nika" არის ცვლადისტვის მნიშვნელობა
+
+surname = "gogishvili"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "tazo" #ეს არის str (string) ტიპის ცვლადი
+
+age = 16 # ეს არის int (integer) მთელი რიცხვი
+height = 180 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean  (bool) ტიპის ცვლადი
+
+knows_programming = True #True ან False
+is_ugly = False #snakecase (წერის ტიპი)
+
+isUgly = False #ჯავასკრიპტული cameLcase
+
+#print(name + " " + surname )
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(know_programming))
+
+#print(name + " " + str(age))
+
+print( "me" " " "var" " " "tazo" " " "gogishvili " " " "16" " " "wlis" " " "simagle" " " "180")
+print("me" " " "var" + " " + name + " " + str(surname) + " " + str(age) + " " "wlis" + " " + "simagle" + " " + str(height))
